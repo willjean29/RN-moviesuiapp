77106e6471c262d5163b07ca41782cc8fe6fd43e
Jean Osco <willjean29@gmail.com>
2023-08-02 00:10:37 -0500

    chore: add navigation and struct folders

---
+++ src/navigation/AppNavigation.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import HomeScreen from '../screens/HomeScreen';
+const Stack = createStackNavigator();
+
+const AppNavigation = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          options={{headerShown: false}}
+          component={HomeScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default AppNavigation;
